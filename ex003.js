a0b9c1d7e9198e97f600a0ff07cfaad2ca39cea0
Joao <jv.prado98@gmail.com>
2023-07-25 10:53:52 -0300

    ex003

--- ex003.js
+++ ex003.js
@@ -1,2 +1,16 @@
-const numeros = [54, 22, 14, 87, 284];
-
+const numeros = [54, 22, 14, 10, 284];
+let i = 0
+let existe = false
+for (num of numeros) {
+    
+    if (num === 10) {
+        existe = true
+        break;
+    }
+    i < numeros.length; i++
+}
+if (existe === true) {
+    console.log(i)
+} else {
+    console.log('-1')
+}
